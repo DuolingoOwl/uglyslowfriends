aa3a0692d852296c9e73ee2946349bea7ffe6a13
DuolingoOwl <64286308+DuolingoOwl@users.noreply.github.com>
2025-04-30 19:19:03 -0700

    Update clicker.js

--- javascript/clicker.js
+++ javascript/clicker.js
@@ -1,40 +1,40 @@
-	document.getElementById('fileInput').addEventListener('change', function(event) {
-    var files = event.target.files;
-var uploadedSkin = event.target.result;
-    var preview = document.getElementById('preview');
-localStorage.setItem("uploadedSkin", uploadedSkin);
-let uploadedSkin = "none"; // current skin uploaded
-    
-    // Clear any existing content
-    preview.innerHTML = '';
+document.getElementById('fileInput').addEventListener('change', function(event) {
+	var files = event.target.files;
+	var uploadedSkin = event.target.result;
+	var preview = document.getElementById('preview');
+	localStorage.setItem("uploadedSkin", uploadedSkin);
+	let uploadedSkin = "none"; // current skin uploaded
+	
+	// Clear any existing content
+	preview.innerHTML = '';
+	
+	img.src = "./images/uglyslowfriend.png"
+	
+	// Loop through all selected files
+	for (var i = 0; i < files.length; i++) {
+		var file = files[i];
 		
-img.src = "./images/uglyslowfriend.png"
-
-    // Loop through all selected files
-    for (var i = 0; i < files.length; i++) {
-      var file = files[i];
-
-      // Only process image files
-      if (!file.type.match('image.*')) {
-        continue;
-      }
-
-      var imgContainer = document.createElement('div');
-      imgContainer.style.marginBottom = '20px'; // Spacing between each image container
-
-	var img = document.createElement('img');
-	      img.src = URL.createObjectURL(file);
-      img.style.height = '256px';
-      img.style.display = 'block'; // Ensure the image is displayed in a block to put it on a new line
-      img.style.marginBottom = '10px';
-
-      // Append the image and file info to the container
-      imgContainer.appendChild(img);
-
-      // Append the container to the preview div
-      preview.appendChild(imgContainer);
-    }
-  });
+		// Only process image files
+		if (!file.type.match('image.*')) {
+			continue;
+		}
+		
+		var imgContainer = document.createElement('div');
+		imgContainer.style.marginBottom = '20px'; // Spacing between each image container
+		
+		var img = document.createElement('img');
+		img.src = URL.createObjectURL(file);
+		img.style.height = '256px';
+		img.style.display = 'block'; // Ensure the image is displayed in a block to put it on a new line
+		img.style.marginBottom = '10px';
+		
+		// Append the image and file info to the container
+		imgContainer.appendChild(img);
+		
+		// Append the container to the preview div
+		preview.appendChild(imgContainer);
+	}
+});
 
 var score = 0;
 
